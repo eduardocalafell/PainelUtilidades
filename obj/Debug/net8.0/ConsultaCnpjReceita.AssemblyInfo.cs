f8efe6dcf84ca5161835504f5554415228f2ff84
educalafell <134091751+eduardocalafell@users.noreply.github.com>
2024-11-02 13:35:08 -0300

    Atualizando build

--- obj/Debug/net8.0/ConsultaCnpjReceita.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsultaCnpjReceita.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultaCnpjReceita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119a377bb3edf93f39f552987f74f4f39ae287e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b748b693fc232d6d4934262c104465a9bb4f77e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultaCnpjReceita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultaCnpjReceita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
